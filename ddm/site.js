99d82d786c0f244dbf61c61f7d53254cea8e8940
mindonfeng <mindonfeng@tencent.com>
2022-01-27 15:46:18 +0800

    viewport adaptive; fix demo data issue

--- ddm/site.js
+++ ddm/site.js
@@ -13,6 +13,10 @@ import './de.js?m=de-dm.js;de-dmm.js;hello::world.js,mindon.js';
 //   console.log(d, 'NOT called');
 // }, 'Hey');
 
+if(screen.width<640) {
+  document.querySelector('meta[name="viewport"]').setAttribute('content',`user-scalable=yes,width=640,scale=${(screen.width/640).toFixed(2)}`);
+}
+
 const dm = document.querySelector('de-dm'),
   dmm = document.querySelector('de-dmm');
 dmm.addEventListener('changed', (evt) => {
@@ -48,22 +52,22 @@ function ddup(data, readonly) {
   dmm.readonly = readonly;
   if (!data) {
     data = `日期,时间,血糖,备注
-    2021-6-4,22:49,7.7,睡前
-    6-4,18:36,8.5,晚餐前
-    6-4,14:34,11,午餐后
-    6-4,11:50,8,午餐前
-    6-4,6:38,4.5,空腹
-    6-3,21:22,6.1,睡前
-    6-3,18:52,9.0,晚餐前
-    6-3,14:53,10.7,午餐后
-    6-3,9:12,7.8,早餐后
-    6-3,6:30,8,空腹
-    6-3,2:4,3.8,低血糖
-    6-2,22:05,6.4,睡前
-    6-2,17:27,12.7,晚餐前
-    6-2,14:17,10.7,午餐后
-    6-2,11:1,5.3,午餐前
-    6-2,7:14,7.9,空腹`;
+    2021-06-04,22:49,7.7,睡前
+    06-04,18:36,8.5,晚餐前
+    06-04,14:34,11,午餐后
+    06-04,11:50,8,午餐前
+    06-04,06:38,4.5,空腹
+    06-03,21:22,6.1,睡前
+    06-03,18:52,9.0,晚餐前
+    06-03,14:53,10.7,午餐后
+    06-03,09:12,7.8,早餐后
+    06-03,06:30,8,空腹
+    06-03,02:04,3.8,低血糖
+    06-02,22:05,6.4,睡前
+    06-02,17:27,12.7,晚餐前
+    06-02,14:17,10.7,午餐后
+    06-02,11:01,5.3,午餐前
+    06-02,07:14,7.9,空腹`;
     p = '2021-06-04,2021-06-03,2021-06-02';
   }
   deqi.apply(dmm, 'load', data, p ? p.split(',') : undefined);
