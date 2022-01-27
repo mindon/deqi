06c03978d18845479053019858b669381988551e
mindonfeng <mindonfeng@tencent.com>
2022-01-27 15:31:05 +0800

    update ddm url in qrcode; add mod.ts for deno deply

---
+++ mod.ts
@@ -0,0 +1,38 @@
+import { serve } from "https://deno.land/std@0.122.0/http/server.ts";
+
+const mimes : {[key: string]: string} = {
+  html: 'text/html; charset=utf-8',
+  css: 'text/css',
+  js: 'application/javascript',
+  jpg: 'image/jpeg',
+  jpeg: 'image/jpeg',
+  png: 'image/png',
+  svg: 'image/svg+xml',
+};
+
+async function handler(request: Request): Promise<Response> {
+  const { pathname } = new URL(request.url);
+  const m = pathname.match(/\.(html|css|js|jpg|jpeg|png|svg)$/);
+  if (m && mimes[m[1]]) {
+    try {
+      const file = await Deno.readFile(`.${pathname}`);
+      return new Response(file, {
+        headers: {
+          "content-type": mimes[m[1]],
+        },
+      });
+    } catch(err)  {
+      if (err instanceof Deno.errors.NotFound) {
+        return new Response("Not found in deqi - from mindon", {status: 404});
+      } else {
+        throw err;
+      }
+    }
+  } else {
+    return new Response("Hello, DeQi - from Mindon");
+  }
+}
+
+serve(handler, {
+  port: 80
+});
