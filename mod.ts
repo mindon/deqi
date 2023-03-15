6a04198ba3f0d12ec4396797b77a1bc650c62d9a
mindon <mindon@gmail.com>
2023-03-15 18:46:36 +0800

    Update mod.ts
    openai chat

--- mod.ts
+++ mod.ts
@@ -1,19 +1,45 @@
 import { serve } from "https://deno.land/std@0.122.0/http/server.ts";
+import { OpenAI } from "https://deno.land/x/openai/mod.ts";
 
-const mimes : {[key: string]: string} = {
-  html: 'text/html; charset=utf-8',
-  css: 'text/css',
-  js: 'text/javascript',
-  jpg: 'image/jpeg',
-  jpeg: 'image/jpeg',
-  png: 'image/png',
-  svg: 'image/svg+xml',
-  csv: 'text/csv',
+const openAI = new OpenAI(Deno.env.get("OPENAI_API_KEY")!);
+
+const mimes: { [key: string]: string } = {
+  html: "text/html; charset=utf-8",
+  css: "text/css",
+  js: "text/javascript",
+  jpg: "image/jpeg",
+  jpeg: "image/jpeg",
+  png: "image/png",
+  svg: "image/svg+xml",
+  csv: "text/csv",
 };
 
 async function handler(request: Request): Promise<Response> {
   let { pathname } = new URL(request.url);
-  if (pathname.endsWith('/')) {
+  if (pathname == "/chat") {
+    if (request.method !== "POST") {
+      return new Response(null, {
+        status: 405,
+        statusText: "Method Not Allowed",
+      });
+    }
+    const messages = await request.json();
+    const completion = await openAI.createChatCompletion({
+      model: "gpt-3.5-turbo-0301",
+      messages,
+      temperature: 0.9,
+      max_tokens: 2048,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.6,
+    });
+    return new Response(JSON.stringify(completion), {
+      headers: {
+        "content-type": "application/json; charset=UTF-8",
+      },
+    });
+  }
+  if (pathname.endsWith("/")) {
     pathname = `${pathname}index.html`;
   }
   const m = pathname.match(/\.(html|css|js|jpg|jpeg|png|svg|csv)$/);
@@ -21,17 +47,17 @@ async function handler(request: Request): Promise<Response> {
     try {
       const file = await Deno.readFile(`.${pathname}`);
       const headers = {
-        'content-type': mimes[m[1]],
+        "content-type": mimes[m[1]],
       };
-      if (pathname.startsWith('/lib/')) {
-        headers['Access-Control-Allow-Origin'] = '*';
+      if (pathname.startsWith("/lib/")) {
+        headers["Access-Control-Allow-Origin"] = "*";
       }
       return new Response(file, {
         headers,
       });
-    } catch(err)  {
+    } catch (err) {
       if (err instanceof Deno.errors.NotFound) {
-        return new Response("Not found in deqi - from mindon", {status: 404});
+        return new Response("Not found in deqi - from mindon", { status: 404 });
       } else {
         throw err;
       }
@@ -42,5 +68,5 @@ async function handler(request: Request): Promise<Response> {
 }
 
 serve(handler, {
-  port: 80
+  port: 80,
 });
