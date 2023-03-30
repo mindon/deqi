a9856090a64eca15af7a0e75077d717c5157f4c5
mindon <mindon@gmail.com>
2023-03-31 03:01:23 +0800

    fix: ai issues

--- ai/openai.js
+++ ai/openai.js
@@ -60,7 +60,7 @@ export class QiChat extends LitElement {
 }
 
 .user {
-  box-shadow: 0 .5rem .8rem rgba(0,0,0,.15);
+  box-shadow: 0 .5rem 1rem rgba(0,0,0,.15);
   margin-bottom: .5rem;
   text-align: right;
   padding-right: 3.2rem;
@@ -75,11 +75,12 @@ export class QiChat extends LitElement {
   z-index: 0;
 }
 .assistant {
-  box-shadow: 0 .5rem 1rem rgba(0,0,0,.15);
+  box-shadow: 0 .5rem .5rem rgba(0,0,0,.15);
   background: #d1ecff;
   margin-bottom: 2rem;
   padding-bottom: 2rem;
 }
+.user > p,
 .assistant > p {
   text-indent: 2.4rem;
   margin: 0;
@@ -92,6 +93,10 @@ export class QiChat extends LitElement {
  word-wrap: break-word;       /* Internet Explorer 5.5+ */
   max-width: 100%;
 }
+.user > p {
+  text-index: 0rem;
+}
+
 .assistant::before {
   width: 24px;
   height: 24px;
@@ -248,7 +253,7 @@ export class QiChat extends LitElement {
       const p = this.renderRoot.host.parentNode;
       q$$('qi-chat', p, (t) => {
         const cl = t.classList;
-        if (cl.contains('fin') || !t.nextSibling || t.nextSibling.tagName != 'QI-CHAT') return;
+        if (cl.contains('fin')) return;
         cl.add('fin');
       });
       renderRoot.host.classList.remove('fin');
@@ -268,7 +273,7 @@ export class QiChat extends LitElement {
       notes.length > 0 
       ? notes.map(note => {
       const {role, content} = note;
-      return html`<div class="${role}">${content}${role == 'assistant'
+      return html`<div class="${role}"><p>${content}</p>${role == 'assistant'
       ? html`<a class="btn continue" title="继续 " @click=${() => this.focus()}>
 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-square-quote-fill" viewBox="0 0 16 16">
   <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-2.5a1 1 0 0 0-.8.4l-1.9 2.533a1 1 0 0 1-1.6 0L5.3 12.4a1 1 0 0 0-.8-.4H2a2 2 0 0 1-2-2V2zm7.194 2.766a1.688 1.688 0 0 0-.227-.272 1.467 1.467 0 0 0-.469-.324l-.008-.004A1.785 1.785 0 0 0 5.734 4C4.776 4 4 4.746 4 5.667c0 .92.776 1.666 1.734 1.666.343 0 .662-.095.931-.26-.137.389-.39.804-.81 1.22a.405.405 0 0 0 .011.59c.173.16.447.155.614-.01 1.334-1.329 1.37-2.758.941-3.706a2.461 2.461 0 0 0-.227-.4zM11 7.073c-.136.389-.39.804-.81 1.22a.405.405 0 0 0 .012.59c.172.16.446.155.613-.01 1.334-1.329 1.37-2.758.942-3.706a2.466 2.466 0 0 0-.228-.4 1.686 1.686 0 0 0-.227-.273 1.466 1.466 0 0 0-.469-.324l-.008-.004A1.785 1.785 0 0 0 10.07 4c-.957 0-1.734.746-1.734 1.667 0 .92.777 1.666 1.734 1.666.343 0 .662-.095.931-.26z"/>
