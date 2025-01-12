05f3db75352a7b02e7efc4930a560e2ff0ed45d5
mindon <mindon@gmail.com>
2025-01-12 17:52:50 +0800

    fix: follow

--- features/daily_test.ts
+++ features/daily_test.ts
@@ -2,6 +2,6 @@ import { follow } from "./daily.ts";
 import { assertEquals } from "jsr:@std/assert";
 
 Deno.test("follow", async () => {
-  const resp = await follow("20250109");
+  const resp = await follow("20250111");
   console.log(await resp.text());
 });
